9fc8cd5f6a7bbe312ffa3aafde0fa60946908e31
Funieru Bogdan (Feby) <fbsoft01@yahoo.com>
2021-12-22 17:01:26 +0200

    Update PostgreSqlClient.cs
    Fix merging error

--- src/Connector.PostgreSqlServer/Connector/PostgreSqlClient.cs
+++ src/Connector.PostgreSqlServer/Connector/PostgreSqlClient.cs
@@ -1,9 +1,42 @@
 using CluedIn.Connector.Common;
 using Npgsql;
+using System;
+using System.Collections.Generic;
 
 namespace CluedIn.Connector.PostgreSqlServer.Connector
 {
-    public interface IPostgreSqlClient : IClientBase<NpgsqlConnection, NpgsqlParameter>
+    public class PostgreSqlClient : ClientBase<NpgsqlConnection, NpgsqlParameter>, IPostgreSqlClient
     {
+        public override string BuildConnectionString(IDictionary<string, object> config)
+        {
+            var connectionStringBuilder = new NpgsqlConnectionStringBuilder
+            {
+                Password = (string)config[CommonConfigurationNames.Password],
+                Username = (string)config[CommonConfigurationNames.Username],
+                Database = (string)config[CommonConfigurationNames.DatabaseName],
+                Host = (string)config[CommonConfigurationNames.Host]
+            };
+
+            connectionStringBuilder.Port = config.TryGetValue(CommonConfigurationNames.PortNumber, out var portEntry) &&
+                                           portEntry != null && portEntry is int port
+                ? port
+                : PostgreSqlServerConstants.DefaultPgSQLPort;
+
+            if (config.TryGetValue(CommonConfigurationNames.Schema, out var schemaNameEntry)
+                && schemaNameEntry is string schemaName && !string.IsNullOrWhiteSpace(schemaName))
+                connectionStringBuilder.SearchPath = schemaName;
+            else
+                connectionStringBuilder.SearchPath = PostgreSqlServerConstants.DefaultPgSQLSchema;
+
+            connectionStringBuilder.SslMode =
+                config.TryGetValue(CommonConfigurationNames.SSLMode, out var SSLModeEntry) && SSLModeEntry != null
+                    ? (SslMode)Enum.Parse(typeof(SslMode), SSLModeEntry.ToString())
+                    : SslMode.Require;
+
+            //// Activate rule for internal debug / force SSLMode to be disabled:
+            //connectionStringBuilder.SslMode = SslMode.Disable;
+
+            return connectionStringBuilder.ToString();
+        }
     }
 }
